18265d0f4c401b90c51b0b1243f536fd748af362
Iridelis40 <134889668+Iridelis40@users.noreply.github.com>
2026-02-08 18:20:01 +0100

    Added function to count nucleotides in a DNA strand

---
+++ DNA.py
@@ -0,0 +1,10 @@
+def count_nuc(seq):
+    nucleotides = {
+        "A":0,
+        "C":0,
+        "G":0,
+        "T":0
+    }
+    for nuc in seq:
+        nucleotides[f"{nuc}"]+=1
+    print(nucleotides)
