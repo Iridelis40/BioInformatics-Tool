d2593ef1240bbbe2bc10dc0b0c3ad96a498ace67
Iridelis40 <134889668+Iridelis40@users.noreply.github.com>
2026-02-09 18:58:57 +0100

    Reverse complement function added

--- DNA.py
+++ DNA.py
@@ -8,3 +8,13 @@ def count_nuc(seq):
     for nuc in seq:
         nucleotides[f"{nuc}"]+=1
     return f"{nucleotides}"
+
+def reverse_complement(seq):
+    rev = []
+    for nuc in seq:
+        rev.insert(0, nuc)
+    complements = {"A":"T","T":"A","C":"G","G":"C"}
+    complementary = []
+    for i in rev:
+        complementary.append(complements[i])
+    return "".join(complementary)
