baf921f25c8224942757d8edadd2bc08aaf024e0
Iridelis40 <134889668+Iridelis40@users.noreply.github.com>
2026-02-15 18:33:11 +0100

    Added function to read FASTA formatted text file

--- DNA.py
+++ DNA.py
@@ -1,4 +1,5 @@
 def count_nuc(seq):
+    #Count nucleotides in a DNA strand
     nucleotides = {
         "A":0,
         "C":0,
@@ -10,6 +11,7 @@ def count_nuc(seq):
     return f"{nucleotides}"
 
 def reverse_complement(seq):
+    #Retrieve de reverse complement of a DNA strand
     rev = []
     for nuc in seq:
         rev.insert(0, nuc)
@@ -20,12 +22,22 @@ def reverse_complement(seq):
     return "".join(complementary)
 
 def GC_content(seq):
+    #Calculate GC content in a DNA sequence
     GC = 0
     for nuc in seq:
         if nuc in ["G","C"]: GC+=1
-    return GC/len(seq)*100
+    return round((GC/len(seq)*100),3)
 
 def read_fasta():
+    #Read a FASTA format and return a dictionary where each element represents an ID (key) with it's string (value)
     with open("fasta.txt", encoding="utf-8") as fasta:
-        content = fasta.read()
-        return content
+        content = fasta.read().replace("\n","")
+        content_list = content.split(">")
+        content_list.pop(0)
+        content_dict = {}
+        for x in content_list:
+            content_dict[f"{x[:13]}"] = f"{x[13:]}"
+        return content_dict
+
+
+        
