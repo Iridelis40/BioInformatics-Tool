79171fe47d31ab87c9afd85a706419b8eb71871b
Iridelis40 <134889668+Iridelis40@users.noreply.github.com>
2026-02-16 19:34:46 +0100

    Added function to calculate Humming distance between two DNA sequences

--- DNA.py
+++ DNA.py
@@ -49,3 +49,9 @@ def GC_content_FASTA(fasta_dict):
         if value == GC_list[0]:
             return f"{key}\n{value}"
     
+def Humming_distance(seq1, seq2):
+    #Return Humming distance (count point mutations between two strands of equal length)
+    distance = 0
+    for i in range(len(seq1)):
+        if seq1[i]!=seq2[i]: distance += 1
+    return distance
